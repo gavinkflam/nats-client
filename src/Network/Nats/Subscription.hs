875efc1e5f8d11834797d67153ea713045fae084
Joao Neves <sevenjp@gmail.com>
2017-04-30 16:41:10 +0200

    Remove unused module

--- src/Network/Nats/Subscription.hs
+++
@@ -1,6 +0,0 @@
-module Network.Nats.Subscription (Subject(..), Subscription, parseSubject) where
-
-import Data.Word (Word8)
-import qualified Data.Attoparsec.ByteString as A
-import qualified Data.ByteString.Char8 as BS
-
